0f26e8bc67be497dd1455f7f9851bee8340cc793
klabautermann3 <lennart.breede@gmail.com>
2021-04-23 11:17:10 -0700

    Re-did year 2020 day 1. Much better now!

--- 2020/python/01/main.py
+++ 2020/python/01/main.py
@@ -1,15 +1,38 @@
-lineList = []
-with open("input.txt") as f:
- 	for line in f:
-		lineList.append(int(line))
+# --- Day 1: Report Repair ---
 
-for a in lineList:
-	for b in lineList:
-		if a + b == 2020:
-			print("The first answer is: " + str(a * b))
+def createExpenseReport(puzzle_input):
+	expense_report = open(puzzle_input, "r").read().split("\n")
+	expense_report = list(map(int, expense_report))
+	return expense_report
 
-for a in lineList:
-	for b in lineList:
-		for c in lineList:
-			if a + b + c == 2020:
-				print("The second answer is: " + str(a * b * c))
+def findPair(expense_report):
+	a = -1
+	b = -1
+	for i in expense_report:
+		for j in expense_report:
+			if i + j == 2020:
+				a = i
+				b = j
+				break
+	return a * b
+
+def findTriple(expense_report):
+	a = -1
+	b = -1
+	c = -1
+	for i in expense_report:
+		for j in expense_report:
+			for k in expense_report:
+				if i + j + k == 2020:
+					a = i
+					b = j
+					c = k
+					break
+	
+	return a * b * c
+
+
+if __name__ == "__main__":
+	expense_report = createExpenseReport("input.txt")
+	print(findPair(expense_report))
+	print(findTriple(expense_report))
