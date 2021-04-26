239f5cef9d85d1ab25e18ec9ce66230248a2de66
klabautermann3 <lennart.breede@gmail.com>
2021-04-26 15:23:25 -0700

    year 2020, day 07, part 1 complete. Stuck at part 2.

---
+++ 2020/python/07/main.py
@@ -0,0 +1,51 @@
+# --- Day 7: Handy Haversacks ---
+
+import time
+
+with open("inputdata.txt", "r") as text:
+    bags = text.read().split("\n")
+
+all_bags = {}
+for b in bags:
+	parent, child = b.split(" bags contain ")
+	if child != "no other bags.":
+		children = child.split(", ")
+		children = [" ".join(child.split(" ")[:3]) for child in children]
+		advanced_children = []
+		for c in children:
+			cc = c.split(" ")
+			amt = int(cc[0])
+			clr = " ".join(cc[1:3])
+			advanced_children.append([amt, clr])
+	else:
+		children = []
+		advanced_children = [[]]
+	all_bags[parent] = advanced_children
+
+
+
+count = 0
+for k, v in all_bags.items():
+	queue = v
+	
+	temp_count = 0
+	for bag in queue:
+		if len(bag) > 0:			
+			if bag[1] == "shiny gold":
+				count += 1
+				break
+			else:
+				for sub_bag in all_bags[bag[1]]:
+					queue.append(sub_bag)
+print(count)
+"""
+super_count = 1
+for b in all_bags["shiny gold"]:
+	if len(b) > 0:
+		super_count += b[0]
+print(super_count)
+"""
+"""
+for k, v in all_bags.items():
+	print("{} -> {}".format(k, v))
+"""
