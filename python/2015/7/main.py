42e1e4913958c94b817d64241abb3739635d2b6b
klabautermann3 <lennart.breede@gmail.com>
2021-12-02 09:08:38 -0800

    started 2015 day 7

---
+++ python/2015/7/main.py
@@ -0,0 +1,19 @@
+# --- Day 7: Some Assembly Required ---
+
+x = 123
+y = 456
+d = x & y
+e = x | y
+f = x << 2
+g = y >> 2
+h = ~x
+i = ~y
+
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(x)
+print(y)
