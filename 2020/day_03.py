e28a965183658dc44f77ad441722cfb37348ae34
lbreede <lennart.breede@gmail.com>
2021-12-16 16:08:54 -0800

    Cleaned up 2020

--- 2020/day_03.py
+++ 2020/day_03.py
@@ -1,4 +1,30 @@
 # --- Day 3: Toboggan Trajectory ---
 
-from aoc_helper import load_input
-linelist = load_input("day03_example.txt")
+line_list = [line.rstrip("\n") for line in open("input.txt")]
+
+def slopecheck(hori, vert):
+	pos = 0
+	found = 0
+	i = 0
+	for line in line_list:
+		if i % vert == 0:
+			if line[pos % len(line)] == "#":
+				found += 1
+			pos += hori
+		i += 1
+	return found
+
+a = slopecheck(1, 1)
+b = slopecheck(3, 1)
+c = slopecheck(5, 1)
+d = slopecheck(7, 1)
+e = slopecheck(1, 2)
+
+"""
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(a*b*c*d*e)
+"""
