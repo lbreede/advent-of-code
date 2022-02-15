daafcfee343da1205072dbccd81932052bfce590
lbreede <lennart.breede@gmail.com>
2022-02-14 18:55:25 -0800

    Started day 7

---
+++ 2016/day_07.py
@@ -0,0 +1,25 @@
+# --- Day 7: Internet Protocol Version 7 ---
+
+
+import re
+
+
+def is_abba(sequence):
+    half = len(sequence) // 2
+
+    start = sequence[:half]
+    end = sequence[half:]
+
+    if start[0] != end[0] and start == end[::-1]:
+        return True
+    return False
+
+
+data = ("abba[mnop]qrst", "abcd[bddb]xyyx", "aaaa[qwer]tyui", "ioxxoj[asdfgh]zxcvbn")
+
+for ip in data:
+    a, b = ip.split("[")
+    b, c = b.split("]")
+
+    if is_abba(a) or is_abba(c) and not is_abba(b):
+        print("ABBA")
