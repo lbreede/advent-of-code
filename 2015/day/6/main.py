bc2b83929dc407489af960b5686f8764c97f01ff
lbreede <lennart.breede@gmail.com>
2022-12-01 15:48:57 -0800

    2022-12-01T15:48

--- 2015/day/6/main.py
+++ 2015/day/6/main.py
@@ -1,1 +1,22 @@
 # --- Day 6: Probably a Fire Hazard ---
+state = [0] * 1_000_000
+brightness = state.copy()
+with open("input.txt") as fp:
+    for line in fp:
+        b, _, a, *c = line.rstrip().split(" ")[::-1]
+        ax, ay = [int(x) for x in a.split(",")]
+        bx, by = [int(x) for x in b.split(",")]
+        c = " ".join(c[::-1])
+        for i in range(ax, bx + 1):
+            for j in range(ay, by + 1):
+                idx = j * 1000 + i
+                if c == "turn on":
+                    state[idx] = 1
+                    brightness[idx] += 1
+                elif c == "turn off":
+                    state[idx] = 0
+                    brightness[idx] = max(brightness[idx] - 1, 0)
+                elif c == "toggle":
+                    state[idx] = 1 - state[idx]
+                    brightness[idx] += 2
+print("Part 1:", sum(state), "\nPart 2:", sum(brightness))
