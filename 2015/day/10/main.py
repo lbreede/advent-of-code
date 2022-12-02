15e467ebb4365e407efa2da43998e628688b85ac
lbreede <lennart.breede@gmail.com>
2022-12-01 23:42:59 -0800

    [FINISHED] 2015 day 10 part 1 + 2

--- 2015/day/10/main.py
+++ 2015/day/10/main.py
@@ -1,1 +1,21 @@
 # --- Day 10: Elves Look, Elves Say ---
+from tqdm import trange
+
+
+def look_and_say(puzzle, iterations):
+    for i in range(iterations):
+        curr_char = temp_puzzle_input = ""
+        for j, char in enumerate(puzzle):
+            if char not in curr_char and j > 0:
+                temp_puzzle_input += str(len(curr_char)) + curr_char[0]
+                curr_char = char
+            else:
+                curr_char += char
+        puzzle = temp_puzzle_input + str(len(curr_char)) + curr_char[0]
+    return puzzle
+
+
+puzzle_input = "1321131112"
+part_one = look_and_say(puzzle_input, 40)
+part_two = look_and_say(part_one, 10)
+print(len(part_one), len(part_two))
