9b265878ea496a45edb25627bcf99be86efcc47d
lbreede <lennart.breede@gmail.com>
2022-12-07 20:28:12 -0800

    new names for better tree visualization

--- 2022/day/7/main.py
+++ 2022/day/7/main.py
@@ -7,24 +7,27 @@ curr_dir = None
 with open("input.txt") as fp:
     for line in fp:
         match_cd = re.search(r"\$ cd (.+)", line)
-        match_file = re.search(r"(\d+)", line)
+        match_file = re.search(r"(\d+) (.+)", line)
         if match_cd:
             arg = match_cd.group(1)
-            curr_dir = Node(arg, parent=curr_dir) if arg != ".." else curr_dir.parent
+            name = f"{arg} (dir)"
+            curr_dir = Node(name, parent=curr_dir) if arg != ".." else curr_dir.parent
         elif match_file:
-            Node(match_file.group(1), parent=curr_dir)
+            size, name = match_file.groups()
+            name = f"{name} (file, size={size})"
+            Node(name, parent=curr_dir)
 
 for pre, _, node in RenderTree(curr_dir.root):
     print(pre + node.name)
 
 sizes = {}
 for leaf in curr_dir.root.leaves:
-    size = int(leaf.name)
+    size = int(leaf.name.split("=")[-1][:-1])
     for a in leaf.ancestors:
         path = "/".join([x.name for x in a.path])
         sizes[path] = size if path not in sizes else sizes[path] + size
 
-disk_space_used = 70_000_000 - sizes["/"]
+disk_space_used = 70_000_000 - sizes["/ (dir)"]
 disk_space_to_clean = 30_000_000 - disk_space_used
 
 part_one = sum([x for x in sizes.values() if x <= 100_000])
