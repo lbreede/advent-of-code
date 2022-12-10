5f23de81d6eb720f5b53f40a2e169e2060062e6d
lbreede <lennart.breede@gmail.com>
2022-12-09 19:05:15 -0800

    Re-solved 2020 day 2 part 1 + 2

---
+++ 2020/day/2/main.py
@@ -0,0 +1,11 @@
+# --- Day 2: Password Philosophy ---
+
+import re
+
+valid_one = valid_two = 0
+with open("input.txt") as fp:
+    for line in fp:
+        a, b, letter, pw = re.search(r"(\d+)-(\d+) (\w): (\w+)", line).groups()
+        valid_one += 1 if int(a) <= pw.count(letter) <= int(b) else 0
+        valid_two += 1 if (pw[int(a) - 1] == letter) ^ (pw[int(b) - 1] == letter) else 0
+print("Part 1:", valid_one, "\nPart 2:", valid_two)
