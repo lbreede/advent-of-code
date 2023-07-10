26da41ed7e30db0ffad30555d7612177c5619c3d
lbreede <lennart.breede@gmail.com>
2023-07-10 14:16:50 -0700

    updated module imports for 2022 day 2

--- rust/2022/day-02/src/bin/part-1.rs
+++ rust/2022/day-02/src/bin/part-1.rs
@@ -1,4 +1,4 @@
-use day_01::process_part1;
+use day_02::process_part1;
 use std::fs;
 
 fn main() {
--- rust/2022/day-02/src/bin/part-2.rs
+++ rust/2022/day-02/src/bin/part-2.rs
@@ -1,4 +1,4 @@
-use day_01::process_part2;
+use day_02::process_part2;
 use std::fs;
 
 fn main() {
